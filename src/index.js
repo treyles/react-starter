d60a75ca84e60d900b68c45c7e5924a5912f091b
Troy Reyles <troyreyles@gmail.com>
2017-11-04 01:09:32 -0700

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './main.scss';
 
-render(<h1 className="title">Hello World.</h1>, document.getElementById('app'));
+render(
+  <h1 className="title">Hello World.</h1>,
+  document.getElementById('root')
+);
